ea06e78840ebd1a2a137c6a192d9daf46c53531c
PC <PC@LAPTOP-RTRLK8VL>
2025-03-14 00:33:34 +0100

    t

--- SteelConnection.Application/Program.cs
+++ SteelConnection.Application/Program.cs
@@ -14,7 +14,7 @@ namespace SteelConnection.Application
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-          //  Application.Run(new MainForm());
+            Application.Run(new MainForm());
         }
     }
 }

7b5890d57ed06e68145bb871a58130a42bee2f7e
PC <PC@LAPTOP-RTRLK8VL>
2025-03-14 00:22:23 +0100

    rtr

--- SteelConnection.Application/Program.cs
+++ SteelConnection.Application/Program.cs
@@ -1,3 +1,6 @@
+using SteelConnection.Application.Views;
+
+
 namespace SteelConnection.Application
 {
     internal static class Program
@@ -11,7 +14,7 @@ namespace SteelConnection.Application
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+          //  Application.Run(new MainForm());
         }
     }
 }
